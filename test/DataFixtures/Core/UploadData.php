bf449f65442cfc1a7589c7ff117d33faaee000c2
Kieran <kieran@supportpal.com>
2020-10-07 13:27:23 +0100

    Updated supportpal/coding-standard (#20)
    * Updated supportpal/coding-standard
    
    * updated composer.json

--- test/DataFixtures/Core/UploadData.php
+++ test/DataFixtures/Core/UploadData.php
@@ -5,14 +5,14 @@ namespace SupportPal\ApiClient\Tests\DataFixtures\Core;
 class UploadData
 {
     public const UPLOAD_DATA = [
-            'id' => 2,
-            'hash' => '4524f5ea967624acdd84f27707088c8b4734637e',
-            'filename' => 'testattachment',
-            'folder' => 'selfservice',
-            'mime' => 'inode/x-empty',
-            'size' => '1.0 KB',
-            'token' => null,
-            'created_at' => 1601571603,
-            'updated_at' => 1601571603
+        'id' => 2,
+        'hash' => '4524f5ea967624acdd84f27707088c8b4734637e',
+        'filename' => 'testattachment',
+        'folder' => 'selfservice',
+        'mime' => 'inode/x-empty',
+        'size' => '1.0 KB',
+        'token' => null,
+        'created_at' => 1601571603,
+        'updated_at' => 1601571603
     ];
 }
