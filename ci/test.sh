842c066be72bf092987cf16e5287a34f69b81917
bytestream <kieran@supportpal.com>
2024-01-21 17:52:57 +0000

    ci: only run test coverage on PHP 8.3

--- ci/test.sh
+++ ci/test.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
-
 set -ex
 
-./vendor/bin/paratest --testsuite $TESTSUITE --coverage-clover clover.xml --stop-on-failure
-./vendor/bin/coverage-check clover.xml $MINCOVERAGE --only-percentage
+if [ -z ${TESTSUITE+x} ]; then
+  echo "Usage: TESTSUITE=unit MINCOVERAGE=100 ${BASH_SOURCE[0]##*/}"
+  exit 1
+fi
+
+args=()
+args+=( "--testsuite=$TESTSUITE" )
+args+=( '--stop-on-failure' )
+[[ "${MINCOVERAGE}" =~ ^[1-9]([0-9]+)?$ ]] && args+=( '--coverage-clover=clover.xml' )
+./vendor/bin/paratest "${args[@]}"
+
+if [[ "${MINCOVERAGE}" =~ ^[1-9]([0-9]+)?$ ]]; then
+  ./vendor/bin/coverage-check clover.xml $MINCOVERAGE --only-percentage
+fi
