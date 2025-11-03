b03424059c5a7e4cae5133a86011b938209db189
Jay <602425+jshah4517@users.noreply.github.com>
2025-11-03 13:49:20 +0000

    fix phpcs and add comments

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -13,28 +13,30 @@ abstract class Model extends \Jenssegers\Model\Model
     /** @var string[] */
     protected $guarded = [];
 
-protected function castAttribute($key, $value)
-{
-    if (is_null($value)) {
-        return $value;
-    }
-
-    $castType = $this->casts[$key] ?? null;
-
-    if ($castType && str_starts_with($castType, 'array:')) {
-        $className = substr($castType, 6);
-
-        if (class_exists($className) && is_array($value)) {
-            return array_map(function ($item) use ($className) {
-                return is_array($item) ? new $className($item) : $item;
-            }, $value);
+    protected function castAttribute($key, $value)
+    {
+        $castType = $this->casts[$key] ?? null;
+        
+        if ($value === null || $castType === null) {
+            return $value;
         }
-    }
 
-    if ($castType && class_exists($castType) && is_array($value)) {
-        return new $castType($value);
-    }
+        // Handle class typing.
+        if ($castType && class_exists($castType) && is_array($value)) {
+            return new $castType($value);
+        }
 
-    return parent::castAttribute($key, $value);
-}
+        // Handle class typing as an array.
+        if ($castType && str_starts_with($castType, 'array:')) {
+            $className = substr($castType, 6);
+
+            if (class_exists($className) && is_array($value)) {
+                return array_map(function ($item) use ($className) {
+                    return is_array($item) ? new $className($item) : $item;
+                }, $value);
+            }
+        }
+
+        return parent::castAttribute($key, $value);
+    }
 }
