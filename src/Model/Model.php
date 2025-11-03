81c7ad25a87d79f08c47c1866ec5a60f6c6cf596
ju5t <m@ju5t.nl>
2025-11-03 13:07:22 +0100

    fix: support model casting

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -12,4 +12,29 @@ abstract class Model extends \Jenssegers\Model\Model
 {
     /** @var string[] */
     protected $guarded = [];
+
+protected function castAttribute($key, $value)
+{
+    if (is_null($value)) {
+        return $value;
+    }
+
+    $castType = $this->casts[$key] ?? null;
+
+    if ($castType && str_starts_with($castType, 'array:')) {
+        $className = substr($castType, 6);
+
+        if (class_exists($className) && is_array($value)) {
+            return array_map(function ($item) use ($className) {
+                return is_array($item) ? new $className($item) : $item;
+            }, $value);
+        }
+    }
+
+    if ($castType && class_exists($castType) && is_array($value)) {
+        return new $castType($value);
+    }
+
+    return parent::castAttribute($key, $value);
+}
 }
